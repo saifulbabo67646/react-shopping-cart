47cacbefc7cd75e519ea63fe9f46ea47068aba2f
saifulbabo67646 <abbo67646@gmail.com>
2021-04-12 11:48:02 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+         <a href='/'>Babus Shopping Cart</a>
+      </header>
+      <main>
+        Product will show here
+      </main>
+      <footer>
+        All Right Reserve.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
