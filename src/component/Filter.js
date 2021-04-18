b74929627416f7172495f0791396bf127c77e8e4
saifulbabo67646 <abbo67646@gmail.com>
2021-04-18 13:06:09 +0600

    add redux with filter in products

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { filterProducts, sortProducts } from '../redux/actions/productActions'
 
-const Filter = ({counts, handleSort, handleFilter, size, sort}) => {
+const Filter = () => {
+   const {items,size,sort,filteredItems} = useSelector(state => state.products)
+   const dispatch = useDispatch()
     return (
         <div className='filter'>
-            <div className='filter-result'>{counts} Products</div>
+            <div className='filter-result'>{items.length} Products</div>
             <div className='filter-sort'>
                 Order {" "}
-                <select value={sort} onChange={handleSort}>
-                    <option>Latest</option>
+                <select value={sort} onChange={e => dispatch(sortProducts(filteredItems, e.target.value))}>
+                    <option value='latest'>Latest</option>
                     <option value='lowest'>Lowest</option>
                     <option value='highest'>Highest</option>
                 </select>
             </div>
             <div className='filter-size'>
                 Filter {" "}
-                <select value={size} onChange={handleFilter}>
+                <select value={size} onChange={(e) => dispatch(filterProducts(items,e.target.value))}>
                     <option value=''>ALL</option>
                     <option value='XS'>XS</option>
                     <option value='S'>S</option>
